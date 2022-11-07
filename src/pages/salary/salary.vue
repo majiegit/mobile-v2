12c354ce7796b580443c4c486eee2706adb15a88
止咳糖浆 <zhangxinliang235@163.com>
2022-11-07 10:03:29 +0800

    薪资明细框

--- src/pages/salary/salary.vue
+++ src/pages/salary/salary.vue
@@ -6,15 +6,16 @@
       <van-col :span="14" class="header_two">
         <div class="time_div">
           <span @click="selectDate('beginDate','开始日期')">
-            {{beginDate}}
+            {{ beginDate }}
           </span>
           <span v-show="beginDate">
             至
           </span>
           <span @click="selectDate('endDate','结束日期')">
-            {{endDate}}
+            {{ endDate }}
           </span>
-          <span style="font-size: 20px; float: right; margin-right: 10px;" @click="selectDate('beginDate','开始日期')"><van-icon name="notes-o"/></span>
+          <span style="font-size: 20px; float: right; margin-right: 10px;" @click="selectDate('beginDate','开始日期')"><van-icon
+              name="notes-o"/></span>
         </div>
       </van-col>
     </van-row>
@@ -28,8 +29,8 @@
       <div class="salary_total_div">
         <van-row type="flex">
           <van-col :span="12" class="item_col" v-for="(item,index) in summarizing" :key=index>
-            <span>{{item.title}} : </span>
-            <span>{{item.number}}</span>
+            <span>{{ item.title }} : </span>
+            <span>{{ item.number }}</span>
           </van-col>
         </van-row>
       </div>
@@ -38,59 +39,68 @@
       <!--薪资数据 标题-->
       <van-row type="flex">
         <van-col :span="24" class="salary_username">
-          <span>{{username}}</span>
-          <span style="font-size: 14px; margin-left: 20px; color: #999999;">{{orgname}}</span>
+          <!--          <span>{{username}}</span>-->
+          <!--          <span style="font-size: 14px; margin-left: 20px; color: #999999;">{{orgname}}</span>-->
         </van-col>
       </van-row>
       <!--薪资数据 内容-->
       <div class="salary_div" v-for="(list,index) in salaryData" :key="index" @click="salaryDetailClick(list)">
         <van-row type="flex">
           <van-col :span="12" class="item_col" style="font-size: 20px;">
-            {{list.salaryList[list.countItem[0]].content.slice(0,7)}}
+            {{ list.salaryList[list.countItem[0]].content.slice(0, 7) }}
           </van-col>
           <van-col :span="12" class="item_col">
             <span>发放日期 :</span>
-            <span>{{list.salaryList[list.countItem[0]].content}}</span>
+            <span>{{ list.salaryList[list.countItem[0]].content }}</span>
           </van-col>
           <van-col :span="12" class="item_col">
-            <span>{{list.salaryList[list.countItem[2]].title}} : </span>
-            <span>{{list.salaryList[list.countItem[2]].content}}</span>
+            <span>{{ list.salaryList[list.countItem[2]].title }} : </span>
+            <span>{{ list.salaryList[list.countItem[2]].content }}</span>
           </van-col>
           <van-col :span="12" class="item_col">
-            <span>{{list.salaryList[list.countItem[1]].title}} : </span>
-            <span>{{list.salaryList[list.countItem[1]].content}}</span>
+            <span>{{ list.salaryList[list.countItem[1]].title }} : </span>
+            <span>{{ list.salaryList[list.countItem[1]].content }}</span>
           </van-col>
           <van-col :span="12" class="item_col">
-            <span>{{list.salaryList[list.countItem[4]].title}} : </span>
-            <span>{{list.salaryList[list.countItem[4]].content}}</span>
+            <span>{{ list.salaryList[list.countItem[4]].title }} : </span>
+            <span>{{ list.salaryList[list.countItem[4]].content }}</span>
           </van-col>
           <van-col :span="12" class="item_col">
-            <span>{{list.salaryList[list.countItem[3]].title}} : </span>
-            <span>{{list.salaryList[list.countItem[3]].content}}</span>
+            <span>{{ list.salaryList[list.countItem[3]].title }} : </span>
+            <span>{{ list.salaryList[list.countItem[3]].content }}</span>
           </van-col>
         </van-row>
       </div>
     </div>
     <div v-else>
-      <van-empty description="暂无数据" />
+      <van-empty description="暂无数据"/>
     </div>
 
     <!--薪资明细弹框-->
     <van-popup
-      v-model="salaryDetailShow"
-      closeable
-      position="top"
-      :style="{ height: '100%',background: 'rgb(245, 245, 245)'}"
+        v-model="salaryDetailShow"
+        round
+        :style="{ height: '70%', width: '90%', background: 'white'}"
     >
-      <div style="width: 96%; height: 92%; margin-top: 12%; padding: 2%; overflow-y: auto;">
+      <div class="top-back1">
+        <div class="top-back2"></div>
+      </div>
+      <div style="">
         <van-row type="flex">
           <van-col span="24" class="detail_title_class">
-            <p>{{salaryDetail.wa_classname}}</p>
-            <van-divider/>
-            <div class="detail_item_class" v-for="(key, index) in salaryDetailKeyList" :key="index">
-              <span  class="detail_item_class_span1">{{salaryDetail.salaryList[key].title}}</span>
-              <span  class="detail_item_class_span2">{{salaryDetail.salaryList[key].content}}</span>
-            </div>
+            <van-row>
+              <van-col span="24" class="salary_title">2022年3月薪资已发放</van-col>
+            </van-row>
+            <van-row style="font-size: 14px;" v-for="(item, index) in salaryDetailKeyList" :key="index">
+              <van-divider :style="index == 0 ?{ borderColor: 'grey', margin: '12px' }: { borderColor: 'grey'}"
+                           dashed></van-divider>
+              <van-col span="16" style="padding-left: 20px;font-size: 16px">{{ item.title }}</van-col>
+              <van-col span="8" style="padding-left: 20px;font-size: 16px">{{ item.content }}</van-col>
+            </van-row>
+            <!--            <div class="detail_item_class" v-for="(key, index) in salaryDetailKeyList" :key="index">-->
+            <!--              <span  class="detail_item_class_span1">{{salaryDetail.salaryList[key].title}}</span>-->
+            <!--              <span  class="detail_item_class_span2">{{salaryDetail.salaryList[key].content}}</span>-->
+            <!--            </div>-->
           </van-col>
         </van-row>
       </div>
@@ -99,10 +109,10 @@
     <!--日期选择框-->
     <van-popup v-model="datePickShow" position="bottom">
       <van-datetime-picker
-        @confirm="confirmDate"
-        v-model="dateModel"
-        type="year-month"
-        :title="'选择'+ fieldName"
+          @confirm="confirmDate"
+          v-model="dateModel"
+          type="year-month"
+          :title="'选择'+ fieldName"
       />
     </van-popup>
   </div>
@@ -113,6 +123,7 @@
   import Header from '@/components/Header/Index'
   // import {fetchData} from 'hr-utils'
   import {Toast} from 'vant';
+  import {querySalaryData} from '@/api/salary'
 
   export default {
     name: 'salary',
@@ -121,11 +132,33 @@
     },
     data() {
       return {
-        salaryDetailShow: false,
+        salaryDetailShow: true,
         salaryDetail: {},
-        salaryDetailKeyList: [],
-        username: getStorage("userinfo").name,
-        orgname: getStorage("userinfo").orgname ,
+        // salaryDetailKeyList: [],
+        salaryDetailKeyList: [
+          {
+            title: '社保',
+            content: '600'
+          },
+          {
+            title: '公积金',
+            content: '800'
+          },
+          {
+            title: '个人所得税',
+            content: '800'
+          },
+          {
+            title: '税前收入',
+            content: '800'
+          },
+          {
+            title: '公积金',
+            content: '800'
+          }
+        ],
+        // username: getStorage("userinfo").name,
+        // orgname: getStorage("userinfo").orgname ,
         salaryData: [],
         summarizing: [],
         field: '',
@@ -148,19 +181,19 @@
     },
     methods: {
 
-      salaryDetailClick(item){
+      salaryDetailClick(item) {
         this.salaryDetail = item
         this.salaryDetailKeyList = Object.keys(item.salaryList)
         console.log(this.salaryDetailKeyList)
         console.log(this.salaryDetail)
         this.salaryDetailShow = true
       },
-      toDecimal2 (x) {
+      toDecimal2(x) {
         var f = parseFloat(x)
         if (isNaN(f)) {
           return false
         }
-        var f = Math.round(x*100)/100
+        var f = Math.round(x * 100) / 100
         var s = f.toString()
         var rs = s.indexOf('.')
         if (rs < 0) {
@@ -172,23 +205,23 @@
         }
         return s
       },
-      totalChange(data){
+      totalChange(data) {
         var shouldTotal = 0
         var practicalTotal = 0
         var chargeTotal = 0
         var taxTotal = 0
-        for( var i = 0; i<data.length; i++){
+        for (var i = 0; i < data.length; i++) {
           shouldTotal += parseFloat(data[i].salaryList.wa_dataf_1.content)
           practicalTotal += parseFloat(data[i].salaryList.wa_dataf_3.content)
           chargeTotal += parseFloat(data[i].salaryList.wa_dataf_2.content)
           taxTotal += parseFloat(data[i].salaryList.wa_dataf_5.content)
         }
-        if(data.length !== 0){
+        if (data.length !== 0) {
           this.summarizing = [
-            {title: '应发汇总',number: this.toDecimal2(shouldTotal)},
-            {title: '实发汇总',number: this.toDecimal2(practicalTotal)},
-            {title: '扣税合计',number: this.toDecimal2(taxTotal)},
-            {title: '扣款合计',number: this.toDecimal2(chargeTotal)}
+            {title: '应发汇总', number: this.toDecimal2(shouldTotal)},
+            {title: '实发汇总', number: this.toDecimal2(practicalTotal)},
+            {title: '扣税合计', number: this.toDecimal2(taxTotal)},
+            {title: '扣款合计', number: this.toDecimal2(chargeTotal)}
           ]
         }
       },
@@ -198,25 +231,34 @@
           message: '加载中...',
           duration: 0
         })
-        fetchData({
-          url: 'hrssc/portal/salary/query',
-          method: 'post',
-          param: {
-            beginDate: this.beginDate,
-            endDate: this.endDate
-          },
-          mock: false,
-          contentType: "application/json",
-          success: res => {
-            this.salaryData = res.data.salaryData
-            this.totalChange(res.data.salaryData)
-            Toast.clear()
-          },
-          error: error => {
-            Toast.clear()
-            Toast(error.message)
-          }
+        let params = {
+          'userId': '1001A9100000000007PX',
+          'beginDate': this.beginDate,
+          'endDate': this.endDate
+        }
+        querySalaryData(params).then(res => {
+          console.log(res)
+          Toast.clear()
         })
+        // fetchData({
+        //   url: 'hrssc/portal/salary/query',
+        //   method: 'post',
+        //   param: {
+        //     beginDate: this.beginDate,
+        //     endDate: this.endDate
+        //   },
+        //   mock: false,
+        //   contentType: "application/json",
+        //   success: res => {
+        //     this.salaryData = res.data.salaryData
+        //     this.totalChange(res.data.salaryData)
+        //     Toast.clear()
+        //   },
+        //   error: error => {
+        //     Toast.clear()
+        //     Toast(error.message)
+        //   }
+        // })
       },
       // 校验日期
       checkDate(date) {
@@ -228,10 +270,10 @@
           let endDateArr = this.endDate.split('-')
           let endDateYear = Number(endDateArr[0])
           let endDateMonth = Number(endDateArr[1])
-          console.log(beginDateYear)
-          console.log(beginDateMonth)
-          console.log(endDateYear)
-          console.log(endDateMonth)
+          // console.log(beginDateYear)
+          // console.log(beginDateMonth)
+          // console.log(endDateYear)
+          // console.log(endDateMonth)
           if (beginDateYear > endDateYear) {
             Toast("开始日期不能大于结束日期")
             return
@@ -293,9 +335,35 @@
     }
   };
 </script>
+<style>
+    .top-back1 {
+        width: 100%;
+        background: #2479ed;
+        height: 20px;
+        position: relative
+    }
 
+    .top-back2 {
+        border-top-left-radius: 16px;
+        border-top-right-radius: 16px;
+        height: 30px;
+        background: white;
+        position: relative;
+        top: 6px;
+    }
+
+    .salary_title {
+        padding: 10px 0 0 20px;
+        font-weight: bolder;
+        font-size: 22px
+    }
+</style>
 
 <style lang='less' scoped>
+  .van-divider {
+    margin: 12px 0px;
+  }
+
   .header_div {
     background: #2479ed;
     height: 55px;
@@ -321,7 +389,6 @@
   }
 
 
-
   .salary_total_div {
     width: 92%;
     min-height: 60px;
@@ -329,8 +396,9 @@
     padding: 4%;
     color: #ffffff;
     border-radius: 5px;
-    box-shadow: 0px 8px 15px 1px rgba(198,198,198,0.3);
+    box-shadow: 0px 8px 15px 1px rgba(198, 198, 198, 0.3);
   }
+
   .salary_div {
     width: 92%;
     margin-top: 10px;
@@ -338,38 +406,48 @@
     background: #fff;
     padding: 4%;
     border-radius: 5px;
-    box-shadow: 0px 8px 15px 1px rgba(198,198,198,0.3);
+    box-shadow: 0px 8px 15px 1px rgba(198, 198, 198, 0.3);
   }
+
   .item_col {
     height: 30px;
     font-size: 14px;
     line-height: 30px;
   }
-  .salary_username{
+
+  .salary_username {
     line-height: 40px;
     font-size: 16px;
   }
-  .detail_title_class{
-    border-radius: 10px;
-    padding: 2%;
+
+  .detail_title_class {
+    //border-radius: 10px;
+    //padding: 20px;
     background: #ffffff;
-    line-height: 30px;
-    font-size: 16px;
-    color: #666;
+    //line-height: 30px;
+    font-size: 20px;
+    color: #000;
+    font-family: "Microsoft YaHei";
+    font-weight: 500;
+    //color: #666;
+
     p {
-      font-size: 18px;
+      font-size: 20px;
       font-weight: bolder;
     }
   }
-  .detail_item_class{
+
+  .detail_item_class {
     width: 100%;
     min-height: 30px;
     line-height: 30px;
     font-size: 14px;
-    &_span1{
+
+    &_span1 {
       float: left;
     }
-    &_span2{
+
+    &_span2 {
       float: right;
     }
   }
