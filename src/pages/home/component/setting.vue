eb43dd85674d850fd96f892b11dcff5526b2f937
止咳糖浆 <zhangxinliang235@163.com>
2023-01-06 14:23:36 +0800

    退出登录框 添加头像部门

--- src/pages/home/component/setting.vue
+++ src/pages/home/component/setting.vue
@@ -1,34 +1,35 @@
 <template>
-  <div>
+  <div class="container">
     <van-popup v-model="show" position="left" :style="{ width: '90%', height: '100%' }" closeable>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;">
+      <div style="margin-top: 40px">
+        <van-row type="flex">
+          <van-col :span="6" offset="2">
+            <van-image
+              style="margin-top: 20px;"
+              round
+              width="70px"
+              height="70px"
+              :src="userInfo.photo"
+            />
+          </van-col>
+          <van-col :span="15" style="margin-top: 4px">
+            <p class="userInfo" v-show='userInfo.name'>
+              <span>
+             {{ userInfo.name }}
+              </span> <br/>
+              <span>{{ userInfo.orgname }}</span> <br/>
+              <span>{{ userInfo.deptname }}</span><br/>
+            </p>
+          </van-col>
+        </van-row>
       </div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div style="width: 100%; height: 30px; position: relative; top: 50px;"></div>
-      <div class="logout" @click="logout">
+      <div class="main" :style="{'height': currentHeight}">
+<!--          <div style="height: 500px"></div>-->
+<!--          <div style="height: 500px"></div>-->
+<!--          <div style="height: 500px"></div>-->
+<!--          <div style="height: 500px"></div>-->
+      </div>
+      <div class="footer" @click="logout">
         <van-button type="info" block>退出登录</van-button>
       </div>
     </van-popup>
@@ -36,37 +37,67 @@
 </template>
 
 <script>
-  import storage from 'store'
-  import {ACCESS_TOKEN, REFRESH_TOKEN,USERINFO} from '@/utils/mutation-types'
-  export default {
-    name: "setting",
-    data() {
-      return {
-        show: false
-      }
+import storage from 'store'
+import { ACCESS_TOKEN, REFRESH_TOKEN, USERINFO } from '@/utils/mutation-types'
+
+export default {
+  name: 'setting',
+  data () {
+    return {
+      userInfo: {},
+      currentHeight: '',
+      show: false
+    }
+  },
+  created () {
+    // console.log(this.userInfo)
+    this.userInfo = storage.get(USERINFO)
+    console.log(this.userInfo)
+    // console.log(userInfo.photo)
+    // localStorage
+  },
+  mounted() {
+    this.currentHeight = (document.documentElement.clientHeight - 100 -94) + 'px'
+  },
+  methods: {
+    // 退出登录
+    logout () {
+      // token 失效,删除token
+      storage.remove(ACCESS_TOKEN)
+      storage.remove(REFRESH_TOKEN)
+      storage.remove(USERINFO)
+      this.$router.push('login')
     },
-    methods: {
-      // 退出登录
-      logout() {
-        // token 失效,删除token
-        storage.remove(ACCESS_TOKEN)
-        storage.remove(REFRESH_TOKEN)
-        storage.remove(USERINFO)
-        this.$router.push("login")
-      },
-      open() {
-        this.show = true
-      },
-      close() {
-        this.show = false
-      }
+    open () {
+      this.show = true
+    },
+    close () {
+      this.show = false
     }
   }
+}
 </script>
 
 <style scoped>
-  .logout {
-    width: 90%;
-    padding: 0px 5%;
-  }
+.container{
+  display: flex;
+  /*布局方向是垂直的*/
+  flex-direction: column;
+  width: 90%;
+  height: 100%;
+}
+.main{
+  flex: 1;
+  width: 90%;
+  overflow-y: auto;
+}
+.footer {
+  width: 90%;
+  height: 44px;
+  padding: 0px 5%;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+}
+
 </style>
